39aff507ea614d5c1741ef22600a35b0b67a616e
Ivan <ivan.kurilin@smail.th-koeln.de>
2024-05-31 14:06:18 +0200

    FirstModel

--- README/README.md
+++ README/README.md
@@ -1,5 +1,8 @@
-pip install matplotlib
-pip install numpy
+1: Install the environment.yml with Anaconda. You will use this interpreter later on
+2: Go to .ipynb files and run them with installed interpreter
+3: Dont forget to provide the right path to the dataset
+
+
 
 To push large files to git:
     - git lfs install
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import tensorflow as tf
+import pandas as pd
+import seaborn as sns
 import pickle
+import random
 import os
 
 
@@ -15,15 +19,35 @@ with open(os.path.join("dataset", "valid.p"), mode='rb') as validation_data:
 X_train, y_train = train['features'], train['labels']
 X_valid, y_valid = valid['features'], valid['labels']
 
-print("Number of training examples: ", X_train.shape[0])
+
+from sklearn.utils import shuffle
+X_train, y_train = shuffle(X_train, y_train)
+X_valid, y_valid = shuffle(X_valid, y_valid)
+
+# Normalize image to [0, 1]
+X_train_norm = X_train / 255
+X_valid_norm = X_valid / 255
+# Check that the images have been correctly converted and normalised
+i = random.randint(1, len(X_train_norm))
+plt.grid(False)
+plt.imshow(X_train[i])
+plt.figure()
+plt.grid(False)
+plt.imshow(X_train_norm[i].squeeze(), cmap = 'gray') # cmap
+from tensorflow.keras import datasets, layers, models
+model = models.Sequential()
+model.summary()
+
+
+
+'''print("Number of training examples: ", X_train.shape[0])
 print("Number of validation examples: ", X_valid.shape[0])
 print("Image data shape =", X_train[0].shape)
-print("Number of classes =", len(np.unique(y_train)))
+print("Number of classes =", len(np.unique(y_train)))'''
 
 # Plotting histograms of the count of each sign
 def histogram_plot(dataset: np.ndarray, label: str):
     """ Plots a histogram of the dataset
-
     Args:
         dataset: The input data to be plotted as a histogram.
         label: The label of the histogram.
