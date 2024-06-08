01cf579c2ef70b7bb51105785e3ac090e5568c82
Dimitrij Pivovar <dimitrij.pivovar@brickmakers.de>
2024-06-08 13:33:15 +0200

    refactor cnn into own file

---
+++ cnn_colored.py
@@ -0,0 +1,62 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+import pandas as pd
+import seaborn as sns
+import pickle
+import random
+import os
+from sklearn.utils import shuffle
+from tensorflow.keras import datasets, layers, models
+
+
+with open(os.path.join("dataset", "train.p"), mode='rb') as training_data:
+    train = pickle.load(training_data)
+with open(os.path.join("dataset", "valid.p"), mode='rb') as validation_data:
+    valid = pickle.load(validation_data)
+
+X_train, y_train = train['features'], train['labels']
+X_valid, y_valid = valid['features'], valid['labels']
+
+X_train, y_train = shuffle(X_train, y_train)
+X_valid, y_valid = shuffle(X_valid, y_valid)
+X_train_norm = X_train / 255
+X_valid_norm = X_valid / 255
+
+i = random.randint(1, len(X_train_norm))
+plt.grid(False)
+plt.imshow(X_train[i])
+plt.figure()
+plt.grid(False)
+plt.imshow(X_train_norm[i].squeeze(), cmap = 'gray') # cmap
+
+model = models.Sequential()
+model.add(layers.Conv2D( filters = 32 , kernel_size = ( 3 , 3), padding = "same" , activation = "relu" , input_shape = ( 32 , 32 , 3)))
+model.add(layers.Conv2D(filters = 64, kernel_size=(3, 3), activation = 'relu'))
+model.add(layers.MaxPooling2D(pool_size=(2, 2)))
+model.add(layers.Dropout(0.25))
+model.add(layers.Flatten())
+model.add(layers.Dense(128, activation='relu'))
+model.add(layers.Dropout(0.5))
+model.add(layers.Dense(43, activation='sigmoid'))
+model.summary()
+model.compile(optimizer = 'Adam', loss = 'sparse_categorical_crossentropy', metrics = ['accuracy'])
+history = model.fit(x = X_train_norm,
+                    y = y_train,
+                    batch_size = 32,
+                    epochs = 10,
+                    verbose = 1,
+                    validation_data = (X_valid_norm, y_valid))
+history.history.keys()
+accuracy = history.history['accuracy']
+val_accuracy = history.history['val_accuracy']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+epochs = range(len(accuracy))
+plt.plot(epochs, loss, 'b', label = 'Training loss')
+plt.plot(epochs, val_loss, 'r', label = 'Validation loss')
+plt.title('Training and Validation loss')
+plt.plot(epochs, accuracy, 'ro', label = 'Training accuracy')
+plt.plot(epochs, val_accuracy, 'r', label = 'Validation accuracy')
+plt.title('Training and Validation accuracy')
+model.save('saved_model/conv_neural_network.keras')
